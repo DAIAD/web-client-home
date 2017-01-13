2422a646fc78b6039643cba4c8d1a55e3bb69c75
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-01-13 16:00:07 +0200

    added pagination to table component

--- components/helpers/Table.js
+++ components/helpers/Table.js
@@ -1,5 +1,6 @@
 const React = require('react');
 const bs = require('react-bootstrap');
+const { IMAGES } = require('../../constants/HomeConstants');
 
 function Table(props) {
   const { className, fields, data } = props;
@@ -10,10 +11,13 @@ function Table(props) {
     throw new Error('Data must be array, check Table render');
   }
   return (
-    <table className={className}>
-      <TableHeader {...props} />
-      <TableBody {...props} />
-    </table>
+    <div>
+      <table className={className}>
+        <TableHeader {...props} />
+        <TableBody {...props} />
+      </table>
+      <Pagination {...props.pagination} />
+    </div>
   );
 }
 
@@ -59,6 +63,46 @@ function TableBody(props) {
         ))
       }
     </tbody>
+  );
+}
+
+function Pagination(props) {
+  const { total, active, onPageClick } = props;
+  return (
+    <div className="pagination" style={{ width: '100%', textAlign: 'center' }}>
+      
+      <div className="navigator">
+        {
+          active > 1 ?
+        <a className="navigator-child pull-left" onClick={() => onPageClick(active - 1)}>
+          <img src={`${IMAGES}/arrow-big-left.svg`} alt="previous" />
+        </a> 
+          :
+         <div />
+        }
+        {
+          total > 0 ? 
+            Array.from({ length: total }, (v, i) => i + 1)
+            .map(page => 
+              page === active ? 
+                <u><a className="pagination-item active" style={{ marginLeft: 10 }} onClick={() => { onPageClick(page); }}>{page}</a></u>
+              :
+              <a className="pagination-item" style={{ marginLeft: 10 }} onClick={() => { onPageClick(page); }}>{page}</a>
+              )
+          :
+           <div />
+         }
+         {
+           active < total ?
+         <a className="navigator-child pull-right" onClick={() => onPageClick(active + 1)}>
+          <img src={`${IMAGES}/arrow-big-right.svg`} alt="next" />
+        </a>
+        :
+          <div />
+        }
+      </div>
+    </div>
+
   );
 }
 
