07069ba9aed444a24ebcaae7342d7d97e6f50d49
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-03-08 14:13:24 +0200

    reverted mistake edit

--- utils/sessions.js
+++ utils/sessions.js
@@ -5,14 +5,14 @@ const { VOLUME_BOTTLE, VOLUME_BUCKET, VOLUME_POOL, ENERGY_BULB, ENERGY_HOUSE, EN
 
 // Returns sessions for AMPHIRO/METER given the DATA API response
 const getDataSessions = function (devices, data) {
-  if (!data || !data.sessions) return [];
+  if (!data) return [];
   
   const devType = getDeviceTypeByKey(devices, data.deviceKey);
   
   if (devType === 'AMPHIRO') {
     return data.sessions;
   } else if (devType === 'METER') {
-    return data.sessions;
+    return data.values;
   }
   return [];
 };
