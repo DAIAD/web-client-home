5a145d378128b4837089064e7c49410222b3383b
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-04-04 14:52:51 +0300

    extracted shower actions to separate modules

--- actions/QueryActions.js
+++ actions/QueryActions.js
@@ -543,111 +543,6 @@ const queryMeterForecastCache = function (options) {
   };
 };
 
-const assignToMember = function (options) {
-  return function (dispatch, getState) {
-    const { deviceKey, sessionId, memberIndex } = options;
-
-    const data = {
-      assignments: [{
-        deviceKey,
-        sessionId,
-        memberIndex,
-        timestamp: new Date().valueOf(),
-      }],
-      csrf: getState().user.csrf,
-    };
-
-    dispatch(requestedQuery());
-
-    return dataAPI.assignToMember(data)
-    .then((response) => {
-      dispatch(receivedQuery(response.success, response.errors));
-      setTimeout(() => { dispatch(resetSuccess()); }, SUCCESS_SHOW_TIMEOUT);
-
-      if (!response || !response.success) {
-        throwServerError(response);  
-      }
-
-      dispatch(clearCacheItems('AMPHIRO', deviceKey, sessionId));
-
-      return response;
-    })
-    .catch((errors) => {
-      console.error('Error caught on assign shower to member:', errors);
-      dispatch(receivedQuery(false, errors));
-      return errors;
-    });
-  };
-};
-
-const ignoreShower = function (options) {
-  return function (dispatch, getState) {
-    const { deviceKey, sessionId } = options;
-
-    const data = {
-      sessions: [{
-        deviceKey,
-        sessionId,
-        timestamp: new Date().valueOf(),
-      }],
-      csrf: getState().user.csrf,
-    };  
-    dispatch(requestedQuery());
-
-    return dataAPI.ignoreShower(data)
-    .then((response) => {
-      dispatch(receivedQuery(response.success, response.errors));
-      setTimeout(() => { dispatch(resetSuccess()); }, SUCCESS_SHOW_TIMEOUT);
-
-      if (!response || !response.success) {
-        throwServerError(response);  
-      }
-      
-      dispatch(clearCacheItems('AMPHIRO', deviceKey, sessionId));
-
-      return response;
-    }) 
-    .catch((errors) => {
-      console.error('Error caught on ignore shower:', errors);
-      dispatch(receivedQuery(false, errors));
-      return errors;
-    });
-  };
-};
-
-const setShowerReal = function (options) {
-  return function (dispatch, getState) {
-    const { deviceKey, sessionId, timestamp } = options;
-
-    const data = {
-      deviceKey,
-      sessionId,
-      timestamp,
-      csrf: getState().user.csrf,
-    };  
-    dispatch(requestedQuery());
-
-    return dataAPI.setShowerReal(data)
-    .then((response) => {
-      dispatch(receivedQuery(response.success, response.errors));
-      setTimeout(() => { dispatch(resetSuccess()); }, SUCCESS_SHOW_TIMEOUT);
-
-      if (!response || !response.success) {
-        throwServerError(response);  
-      }
-      
-      dispatch(clearCacheItems('AMPHIRO', deviceKey, sessionId));
-
-      return response;
-    }) 
-    .catch((errors) => {
-      console.error('Error caught on set shower real:', errors);
-      dispatch(receivedQuery(false, errors));
-      return errors;
-    });
-  };
-};
-
 const queryUserComparisons = function (month, year) {
   return function (dispatch, getState) {
     const data = {
@@ -897,7 +792,4 @@ module.exports = {
   queryDataAverageCache,
   fetchWaterIQ,
   fetchUserComparison,
-  assignToMember,
-  ignoreShower,
-  setShowerReal,
 };
--- api/data.js
+++ api/data.js
@@ -6,16 +6,7 @@ const DataAPI = {
   },
   getComparisons: function ({ csrf, month, year }) {
     return callAPI(`/action/comparison/${year}/${month}`, { csrf }, 'GET');
-  },
-  assignToMember: function (data) {
-    return callAPI('/action/data/session/member', data);
-  },
-  ignoreShower: function (data) {
-    return callAPI('/action/data/session/ignore', data);
-  },
-  setShowerReal: function (data) {
-    return callAPI('/action/data/session/date', data);
-  },
+  }, 
 };
 
 module.exports = DataAPI;
