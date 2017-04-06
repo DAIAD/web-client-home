b996b27f3c29442b370eb837fab81e37ad61e019
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-04-06 13:48:40 +0300

    added package json, exported modules and babelified

---
+++ exports.js
@@ -0,0 +1,11 @@
+module.exports = {
+  actions: {
+    QueryActions: require('./actions/query/QueryActionsConnector'),
+  },
+  components: {
+    Widgets: require('./components/helpers/Widgets'),
+  },
+  utils: {
+    widgets: require('./utils/widgets'),
+  },
+};
